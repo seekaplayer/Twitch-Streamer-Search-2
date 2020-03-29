7c4f3c542651f727f42cd92f3b13ddbc09652ccd
Seekaplayer <44986525+seekaplayer@users.noreply.github.com>
2020-03-29 19:48:04 -0400

    added profile image

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -9,13 +9,39 @@ const Profile = ({ profileData, isLive }) => {
           {isLive ? (
             <TwitchPlayer username={profileData.display_name} />
           ) : (
-            "You're not live!"
+            <img
+              className="img-fluid"
+              src={
+                profileData.video_banner
+                  ? profileData.video_banner
+                  : "/images/twichdefaultbg.jpg"
+              }
+            />
           )}
         </div>
       </div>
       <div className="row mt-3">
-        <div className="col-4"></div>
-        <div className="col-8">{profileData.description}</div>
+        <div className="col-md-4 order-2 order-md-1">
+          <img
+            className="rounded-circle img-fluid profile_image mx-auto"
+            src={profileData.logo}
+          />
+        </div>
+        <div className="col-md-8 order-md-2">
+          <div className="card">
+            <div className="card-body">
+              <h2 className="text-center">
+                {profileData.display_name}
+                {profileData.partner && (
+                  <img className="verified_badge" src="/images/verified.png" />
+                )}
+              </h2>
+
+              <hr />
+              <p>{profileData.description}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
